c9ae166fabe607f8db62cb33b37ca890e4e4c75f
Richard Iyama <richardiyama@yahoo.com>
2017-01-12 20:09:19 +0100

    commit

---
+++ MusicHouse/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MusicHouse.Startup))]
+namespace MusicHouse
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ MusicHouse/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using MusicHouse.Models;
+
+namespace MusicHouse.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public List<Cart> CartItems { get; set; }
+        public decimal CartTotal    { get; set; }
+    }
+}
