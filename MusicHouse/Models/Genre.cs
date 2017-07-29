95a3847d0b5bebbb5d17a66aa8f2e6cbd1d3c55c
Richard Iyama Oluwaseun <richardiyama@yahoo.com>
2017-07-29 05:45:41 +0100

    Update Genre.cs

--- MusicHouse/Models/Genre.cs
+++ MusicHouse/Models/Genre.cs
@@ -4,9 +4,12 @@ namespace MusicHouse.Models
 {
     public  class Genre
     {
+        
         public virtual int      GenreId     { get; set; }
         public virtual string   Name        { get; set; }
         public virtual string   Description { get; set; }
+       
         public virtual List<Album> Albums   { get; set; }
+   
     }
 }
