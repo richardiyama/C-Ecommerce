4fdb494dd04babe60b9a76b34429115f5d8df393
Richard Iyama Oluwaseun <richardiyama@yahoo.com>
2017-07-29 05:44:16 +0100

    Update Album.cs

--- MusicHouse/Models/Album.cs
+++ MusicHouse/Models/Album.cs
@@ -29,8 +29,10 @@ namespace MusicHouse.Models
         [StringLength(1024)]
         public string AlbumArtUrl { get; set; }
 
-        public virtual Genre Genre { get; set; }
+       
+       public virtual Genre Genre { get; set; }
         public virtual Artist Artist { get; set; }
         public virtual List<OrderDetail> OrderDetails { get; set; }
+    
     }
 }
