34bf34418a34d716d81904c8c2872a8a692c5982
Richard Iyama Oluwaseun <richardiyama@yahoo.com>
2017-07-29 05:48:54 +0100

    Update ShoppingCartRemoveViewModel.cs

--- MusicHouse/ViewModels/ShoppingCartRemoveViewModel.cs
+++ MusicHouse/ViewModels/ShoppingCartRemoveViewModel.cs
@@ -2,10 +2,13 @@
 {
     public class ShoppingCartRemoveViewModel
     {
+        
         public string Message       { get; set; }
         public decimal CartTotal    { get; set; }
         public int CartCount        { get; set; }
         public int ItemCount        { get; set; }
+       
         public int DeleteId         { get; set; }
+    
     }
 }
